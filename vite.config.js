c2e8f63f9637023658a89d63b5c1a47ffa604651
Aakash Verma <dev.aakashv@gmail.com>
2023-07-25 12:33:10 +0530

    Resolve the `eslinter` issues

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: 'src',
+  root: 'src',
 });
